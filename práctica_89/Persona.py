5e94354763b5f12d777adf2da811bd20f5305e54
Lau Zarate <166766762+lauzarg@users.noreply.github.com>
2024-07-02 11:13:10 -0300

    agregado parametros

--- práctica_89/Persona.py
+++ práctica_89/Persona.py
@@ -1,4 +1,11 @@
 class Persona:
-    pass
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = "Juan"
+        self.apellido = "Gonzalez"
+        self.edad = "20"
 
-print (type(persona))
+persona1 = Persona ()
+
+print (persona1.nombre)
+print (persona1.apellido)
+print (persona1.edad)
